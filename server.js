3a7e0036250a5e1e3f1c231af4503e65846965a3
Marcio Galli <taboca@gmail.com>
2018-08-08 18:30:07 -0300

    Server

---
+++ server.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const express    = require('express');
+const path       = require('path');
+const fs         = require('fs');
+const app        = express();
+
+app.get('/doc/pageA', function (req, res, next) {
+  var options = {
+    root: __dirname + '/build/',
+  };
+
+  var fileName = 'index.html';
+  res.sendFile(fileName, options, function (err) {
+    if (err) {
+      next(err);
+    } else {
+      console.log('Sent:', fileName);
+    }
+  });
+});
+
+app.use('/', express.static(path.join(__dirname, 'build')));
+
+const PORT = process.env.PORT || 8080;
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
+
+module.exports = app;
